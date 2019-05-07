cea0b62a676636d8a3bdc5bf348bb14d8dc8319f
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2019-05-07 14:27:15 +0800

    add homepage

---
+++ michelleversille/precache-manifest.288c4576d01571e5768d6f70edef5b23.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ebc61e0fe6c59de56205e5b86982a3",
+    "url": "/michelleversille/index.html"
+  },
+  {
+    "revision": "12c41ba756ee0d7e858d",
+    "url": "/michelleversille/static/css/main.37e1763b.chunk.css"
+  },
+  {
+    "revision": "262be7f4a27edf5abdaa",
+    "url": "/michelleversille/static/js/2.bd730b44.chunk.js"
+  },
+  {
+    "revision": "12c41ba756ee0d7e858d",
+    "url": "/michelleversille/static/js/main.7bf6c7e1.chunk.js"
+  },
+  {
+    "revision": "d04fe031287b7889a2f2",
+    "url": "/michelleversille/static/js/runtime~main.02bc2ef6.js"
+  },
+  {
+    "revision": "c9f3165c9d0300aebf43f198d7c19ae8",
+    "url": "/michelleversille/static/media/collage1.c9f3165c.png"
+  },
+  {
+    "revision": "d5b1d6b6af405945d5ee024ec8fb6b42",
+    "url": "/michelleversille/static/media/collage2.d5b1d6b6.png"
+  },
+  {
+    "revision": "afa5c981641a52757dc03f8d2f17015a",
+    "url": "/michelleversille/static/media/collage3.afa5c981.png"
+  },
+  {
+    "revision": "41bf8b6ddbcdb30789af496e7e1847b2",
+    "url": "/michelleversille/static/media/logo-1.41bf8b6d.png"
+  },
+  {
+    "revision": "893deec161b53841d5664c486187c7cd",
+    "url": "/michelleversille/static/media/logo-2.893deec1.png"
+  },
+  {
+    "revision": "b71d965b998123cb368adcc09bf8ffa8",
+    "url": "/michelleversille/static/media/logo-3.b71d965b.png"
+  },
+  {
+    "revision": "3ed3817ec943d5d71d5fd5ac580da841",
+    "url": "/michelleversille/static/media/logo-4.3ed3817e.png"
+  },
+  {
+    "revision": "0ab0f4c0dc5ffcdb9caf6acfd04ecb14",
+    "url": "/michelleversille/static/media/logo-5.0ab0f4c0.png"
+  }
+]);
--- michelleversille/service-worker.js
+++ michelleversille/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b08da3db2859763fe5422b63ce0e5fba.js"
+  "/michelleversille/precache-manifest.288c4576d01571e5768d6f70edef5b23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/michelleversille/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
