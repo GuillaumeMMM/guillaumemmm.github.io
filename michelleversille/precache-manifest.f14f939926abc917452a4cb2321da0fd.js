633b1008099e6f9076990c5ad118f1dd2b09e848
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2019-05-07 12:51:05 +0800

    michelleversille website

---
+++ michelleversille/precache-manifest.f14f939926abc917452a4cb2321da0fd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f66e7fc515c855a5b92b069aa9b277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "503ba8a3bd45ab0d71b5",
+    "url": "/static/css/main.37e1763b.chunk.css"
+  },
+  {
+    "revision": "274a09f7215d47ec602f",
+    "url": "/static/js/2.b6746548.chunk.js"
+  },
+  {
+    "revision": "503ba8a3bd45ab0d71b5",
+    "url": "/static/js/main.8f978ee4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9f3165c9d0300aebf43f198d7c19ae8",
+    "url": "/static/media/collage1.c9f3165c.png"
+  },
+  {
+    "revision": "d5b1d6b6af405945d5ee024ec8fb6b42",
+    "url": "/static/media/collage2.d5b1d6b6.png"
+  },
+  {
+    "revision": "afa5c981641a52757dc03f8d2f17015a",
+    "url": "/static/media/collage3.afa5c981.png"
+  },
+  {
+    "revision": "41bf8b6ddbcdb30789af496e7e1847b2",
+    "url": "/static/media/logo-1.41bf8b6d.png"
+  },
+  {
+    "revision": "893deec161b53841d5664c486187c7cd",
+    "url": "/static/media/logo-2.893deec1.png"
+  },
+  {
+    "revision": "b71d965b998123cb368adcc09bf8ffa8",
+    "url": "/static/media/logo-3.b71d965b.png"
+  },
+  {
+    "revision": "3ed3817ec943d5d71d5fd5ac580da841",
+    "url": "/static/media/logo-4.3ed3817e.png"
+  },
+  {
+    "revision": "0ab0f4c0dc5ffcdb9caf6acfd04ecb14",
+    "url": "/static/media/logo-5.0ab0f4c0.png"
+  }
+]);
