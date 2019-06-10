37636163cf8e7919cbf608b7b34fb18f374447d6
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2019-06-10 19:36:15 +0800

    ocean plastic challenge folder

---
+++ oceanplastic/precache-manifest.063506faeace987094ee451f2771ceaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f77612dd5382c2f209f8c7f705d115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dae18d1b7a8c018d5c0",
+    "url": "/static/css/main.5dd3fd7b.chunk.css"
+  },
+  {
+    "revision": "3de4ac22855f4d0f8d8d",
+    "url": "/static/js/2.e0b0020e.chunk.js"
+  },
+  {
+    "revision": "5dae18d1b7a8c018d5c0",
+    "url": "/static/js/main.5f2944ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46ae72d18af495e5172a43c0804da04b",
+    "url": "/static/media/1w.46ae72d1.png"
+  },
+  {
+    "revision": "827a222f2bc01887de4499930ffd721a",
+    "url": "/static/media/2a.827a222f.png"
+  },
+  {
+    "revision": "0074628b5f58eb40ba6d404f7b4a0f0a",
+    "url": "/static/media/2ic.0074628b.png"
+  },
+  {
+    "revision": "9d932a3a4106cf7088eb782081e4514c",
+    "url": "/static/media/2w.9d932a3a.png"
+  },
+  {
+    "revision": "a300f98486a655704247afeef0b96e9e",
+    "url": "/static/media/3a.a300f984.png"
+  },
+  {
+    "revision": "57051517c81a5c38485cc30abd52f7b2",
+    "url": "/static/media/3w.57051517.png"
+  },
+  {
+    "revision": "0997ee64c6b5c560fc477e737eee7ccc",
+    "url": "/static/media/bg.0997ee64.jpeg"
+  }
+]);
